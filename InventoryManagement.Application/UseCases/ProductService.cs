4546da1561990a21e6abc4db305fe71045cb16ae
mario-alexx <marioatoroh@gmail.com>
2025-02-03 17:58:52 -0300

    feat(usecases): implement use cases

---
+++ InventoryManagement.Application/UseCases/ProductService.cs
@@ -0,0 +1,24 @@
+﻿using InventoryManagement.Application.Interfaces;
+using InventoryManagement.Domain.Entities;
+
+namespace InventoryManagement.Application.UseCases
+{
+    public class ProductService
+    {
+        private readonly IRepository<Product> _productRepository;
+        public ProductService(IRepository<Product> productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProductsAsync() => await _productRepository.GetAllAsync();
+
+        public async Task<Product?> GetProductByIdAsync(int id) => await _productRepository.GetByIdAsync(id);
+
+        public async Task AddProductAsync(Product product) => await _productRepository.AddAsync(product);
+
+        public void UpdateProduct(Product product) => _productRepository.Update(product);
+
+        public void DeleteProduct(Product product) => _productRepository.Delete(product);
+    }
+}
---
+++ InventoryManagement.Application/UseCases/SupplierService.cs
@@ -0,0 +1,25 @@
+﻿using InventoryManagement.Application.Interfaces;
+using InventoryManagement.Domain.Entities;
+
+namespace InventoryManagement.Application.UseCases
+{
+    public class SupplierService
+    {
+        private readonly IRepository<Supplier> _supplierRepository;
+
+        public SupplierService(IRepository<Supplier> supplierRepository)
+        {
+            _supplierRepository = supplierRepository;
+        }
+
+        public async Task<IEnumerable<Supplier>> GetAllSuppliersAsync() => await _supplierRepository.GetAllAsync();
+
+        public async Task<Supplier?> GetSupplierByIdAsync(int id) => await _supplierRepository.GetByIdAsync(id);
+
+        public async Task AddSupplierAsync(Supplier supplier) => await _supplierRepository.AddAsync(supplier);
+
+        public void UpdateSupplier(Supplier supplier) => _supplierRepository.Update(supplier);
+
+        public void DeleteSupplier(Supplier supplier) => _supplierRepository.Delete(supplier);
+    }
+}
